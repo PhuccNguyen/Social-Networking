3e9cd533f615897ae44933e574c3e05294fb7132
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-05-21 04:02:35 +0700

    add apis  new

--- Server/index.js
+++ Server/index.js
@@ -11,4 +11,29 @@ import { fileURLToPath } from 'url';
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.confg();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginRsourcesPolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+    // /* FILE STORAGE */ MongoDB
+const storage = multer.diskStorage({
+ destination: function (req, file, cb) {
+  cb(null, "public/assets");
+ },
+ filename: function (req, file, cb) {
+    cb(null, file.originalname);
+ }
+});
+const upload = multer ({storage});
+
+
+
 
--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
+
+
+const express = require('express');
+const path = require('path');
+
+// import express from 'express'
+const app = express()   
 const port = 8080
 
+//  declare routes
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/aa', (req, res) => {
+    // res.send('<h2>Nice!<h2>')
+    res.render('abs.html')
+  })
+
+// config teamples engine
+app.set('views',  path.join(__dirname, 'views'));
+app.set('views engine', 'ejs')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
