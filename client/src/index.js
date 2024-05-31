3c5460ecd0e3e1d650dbc94c255a2a8dca026d18
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-06-01 00:26:31 +0700

    add new page

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,37 @@ import App from './App.js"';
 import authReducer from './state';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+
 import { persistStore, persistReducer, FLUSH, REHYDRATE, 
     PAUSE, PERSIST,PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const app = {};
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer ( persistConfig, authReducer );
+
+const store = configureStore ({
+    reducer: persistedReducer ,
+    middleware: ( getDefaultMiddleware) => 
+        getDefaultMiddleware ({
+            serializableCheck: {
+                ignoreActions: [ FLUSH, REHYDRATE, 
+                    PAUSE, PERSIST,PURGE, REGISTER ]
+            },
+        }),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Provider store={store} >
+            <PersistGate loading={null} persistor={persistStore(store)}>
+                <App>
+
+                </App>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
+)
+
+
