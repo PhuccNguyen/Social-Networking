a03c0043b14fe6d1a751426bd778cc12c47c7fbe
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-06-01 00:53:54 +0700

    add new function

---
+++ client/src/components/middleinsert.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import { Styled } from "@mui/system";
+
+const middleinsert = style(Box)({
+    display: "flex",
+    justifyContent: "Space-between",
+    alignItems: "center",
+})
+
+
+export default middleinsert;
