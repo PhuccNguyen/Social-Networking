e08c75953f4b26c973ae704ab8fc8e3e49224504
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-06-09 00:36:25 +0700

    add new code

---
+++ client/src/homePage/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// import { Hand } from "@mui/material";
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+        </div>
+    );
+}
