6d402aaf6564b0f446ee6146d59a24dc96efc1f2
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-07-19 03:19:46 +0700

    add new database

--- client/src/views/widgets/UserWidget.jsx
+++ client/src/views/widgets/UserWidget.jsx
@@ -1,4 +1,23 @@
 import {
     ManageAccountsOutined,
-    Notice,
+    EditOutlined,
+    LocationOnOutlined,
+    WorkOutlinedOutlined,
 } from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material"
+import Imageuser from "components/imageuser";
+import AdjustContent from "components/adjustcontent";
+import widgetwrapper from "components/widgetwrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const UserWidget = ({ userId, picturePath }) => {
+    const [ user, setUser ] = useState(null);
+    const { palette } = useTheme();
+    const navigate = useNavigate();
+    const token = useSelector((state) => state.token);
+    const dark = palette.neutral.dark;
+    const medium = palette.neutral.medium;
+    const main = palette.neutral.main;
+}
