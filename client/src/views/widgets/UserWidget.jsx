a758d37647577df770be7c53479af0adad4b9043
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-07-26 13:48:04 +0700

    add new database

--- client/src/views/widgets/UserWidget.jsx
+++ client/src/views/widgets/UserWidget.jsx
@@ -51,20 +51,96 @@ const UserWidget = ({ userId, picturePath }) => {
 
   return (
     <widgetwrapper>
-       {/* FIRST ROW */}
-       
-       <AdjustContent>
-       <h2> Chadasd </h2>
-        <AdjustContent gap="1rem">
-         <Box>
-            <Typography>
-                {userName}
-            </Typography>
 
+       {/******** One *******/}
+       <AdjustContent
+        gap="0.5rem"
+        pd="1.1rem"
+        onClick={() => navigate (`/profile/${userId}`)}>
+        <AdjustContent gap="1rem">
+          <UserImage image={picturePath} />  
+         <Box>
+            <Typography
+            variant="h4"
+            color={dark}
+            fontWeight="500"
+            sx = {{ "&:hover": 
+               {
+                color: palette.primary.light,
+                cursor: "pointer",
+               },
+            }}>
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}> {friends.length} Friends </Typography>
          </Box>
-       <h2> Chadasd </h2>
         </AdjustContent>
+        <ManageAccountsOutlined/>
        </AdjustContent>
+       <Divider />
+
+       {/******** Two *******/}
+       <Box pb="1rem 0 "  >
+       <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+          <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={medium}>{location}</Typography>
+        </Box>
+        <Box display="flex" alignItems="center" gap="1rem">
+          <WorkOutlineOutlined fontSize="large" sx= {{ color: main}}/>
+          <Typography color={medium}> {occupation} </Typography>
+        </Box>
+       </Box>
+       <Divider />
+
+       {/******** Three *******/}
+       <Box p="1rem 0">
+        <AdjustContent mb="0.5rem">
+          <Typography color={medium}>Who's   viewed your profile</Typography>
+          <Typography color={main} fontWeight="500">
+            {viewedProfile}
+          </Typography>
+        </AdjustContent>
+        <AdjustContent>
+          <Typography color={medium}>Impressions of your post</Typography>
+          <Typography color={main} fontWeight="500">
+            {impressions}
+          </Typography>
+        </AdjustContent>
+      </Box>
+      <Divider />
+             {/******** Four *******/}
+             <Box p="1rem 0">
+        <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+          Social Profiles
+        </Typography>
+
+        <AdjustContent gap="1rem" mb="0.5rem">
+          <AdjustContent gap="1rem">
+            <img src="../assets/twitter.png" alt="twitter" />
+            <Box>
+              <Typography color={main} fontWeight="500">
+                Twitter
+              </Typography>
+              <Typography color={medium}>Social Network</Typography>
+            </Box>
+          </AdjustContent>
+          <EditOutlined sx={{ color: main }} />
+        </AdjustContent>
+
+        <AdjustContent gap="1rem">
+          <AdjustContent gap="1rem">
+            <img src="../assets/linkedin.png" alt="linkedin" />
+            <Box>
+              <Typography color={main} fontWeight="500">
+                Linkedin
+              </Typography>
+              <Typography color={medium}>Network Platform</Typography>
+            </Box>
+          </AdjustContent>
+          <EditOutlined sx={{ color: main }} />
+        </AdjustContent>
+      </Box>
+
 
     </widgetwrapper>
   )
