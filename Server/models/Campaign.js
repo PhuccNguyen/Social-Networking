6636038fc183a9274383534f3e508c9ea5d99c0f
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-10-17 03:29:27 +0700

    add models campains

--- Server/models/Campaign.js
+++ Server/models/Campaign.js
@@ -1,60 +1,60 @@
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
-// const CampaignSchema = new mongoose.Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   description: {
-//     type: String,
-//     required: true,
-//   },
-//   registrationStartDate: {
-//     type: Date,
-//     required: true,
-//   },
-//   registrationEndDate: {
-//     type: Date,
-//     required: true,
-//   },
-//   maxVolunteers: {
-//     type: Number,
-//     required: true,
-//   },
-//   location: {
-//     type: String,
-//     required: true,
-//   },
-//   campaignStartDate: {
-//     type: Date,
-//     required: true,
-//   },
-//   campaignStartTime: {
-//     type: String,
-//     required: true,
-//   },
-//   campaignEndDate: {
-//     type: Date,
-//     required: true,
-//   },
-//   campaignEndTime: {
-//     type: String,
-//     required: true,
-//   },
-//   milestones: {
-//     type: Array,
-//     default: [],
-//   },
-//   image: {
-//     type: String,  // This stores the image filename
-//     default: null,
-//   },
-//   createdBy: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true,
-//   },
-// }, { timestamps: true });
+const CampaignSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  registrationStartDate: {
+    type: Date,
+    required: true,
+  },
+  registrationEndDate: {
+    type: Date,
+    required: true,
+  },
+  maxVolunteers: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  campaignStartDate: {
+    type: Date,
+    required: true,
+  },
+  campaignStartTime: {
+    type: String,
+    required: true,
+  },
+  campaignEndDate: {
+    type: Date,
+    required: true,
+  },
+  campaignEndTime: {
+    type: String,
+    required: true,
+  },
+  milestones: {
+    type: Array,
+    default: [],
+  },
+  image: {
+    type: String,  // This stores the image filename
+    default: null,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
 
-// const Campaign = mongoose.model('Campaign', CampaignSchema);
-// export default Campaign;
+const Campaign = mongoose.model('Campaign', CampaignSchema);
+export default Campaign;
