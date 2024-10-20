7cf99c1ec4185fc7cb50cdb4dcfc76170dda47fe
PhuccNguyen <nguyenhoangphuc315@gmail.com>
2024-10-21 02:55:15 +0700

    add contrller post image after fix error

--- client/src/views/VolunteerPage/index.jsx
+++ client/src/views/VolunteerPage/index.jsx
@@ -1,43 +1,43 @@
-// import { Box, useMediaQuery } from '@mui/material'; 
-// import Navbar from "views/navbar";
-// import { useSelector } from 'react-redux';
-// import UserWidget from 'views/widgets/UserWidget';
-// import MyPostWidget from "views/widgets/MyPostWidget";
-// import CampaignWidget from "views/widgets/CampaignWidget";
-// import EventVolunteer from "views/widgets/VolunteerAdsEvent";
-// import VolunteerPost from "views/widgets/VolunteerPost";
-// import VolunteerPostsWidget from "views/widgets/PostsWidget";
+import { Box, useMediaQuery } from '@mui/material'; 
+import Navbar from "views/navbar";
+import { useSelector } from 'react-redux';
+import UserWidget from 'views/widgets/UserWidget';
+import MyPostWidget from "views/widgets/MyPostWidget";
+import CampaignWidget from "views/widgets/CampaignWidget";
+import EventVolunteer from "views/widgets/VolunteerAdsEvent";
+import VolunteerPost from "views/widgets/VolunteerPost";
+import VolunteerPostsWidget from "views/widgets/PostsWidget";
 
-// const VolunteerPage = () => {
-//   const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
-//   const { _id, picturePath, userName, role } = useSelector((state) => state.user); // Ensure 'role' is in state
+const VolunteerPage = () => {
+  const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath, userName, role } = useSelector((state) => state.user); // Ensure 'role' is in state
 
-//   return (
-//     <Box>
-//       <Navbar />
-//       <Box
-//         width="100%"
-//         padding="2rem 6%"
-//         display={isNonMobileScreen ? "flex" : "block"}
-//         gap="0.5rem"
-//         justifyContent="space-between"
-//         marginTop="70px"
-//       >
-//         <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
-//           <UserWidget userId={_id} picturePath={picturePath} userName={userName} role={role}/>
-//         </Box>
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreen ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+        marginTop="70px"
+      >
+        <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} userName={userName} role={role}/>
+        </Box>
         
-//         <Box flexBasis={isNonMobileScreen ? "26%" : undefined} mt={isNonMobileScreen ? undefined : "2rem"}>
-//         <VolunteerPost picturePath={picturePath} role={role} />
-//         <CampaignWidget userId={_id}  />
-//         </Box>
+        <Box flexBasis={isNonMobileScreen ? "26%" : undefined} mt={isNonMobileScreen ? undefined : "2rem"}>
+        <VolunteerPost picturePath={picturePath} role={role} />
+        <CampaignWidget userId={_id}  />
+        </Box>
 
-//         <Box flexBasis="26%" maxHeight="80vh" overflow="auto">
-//           <EventVolunteer />
-//         </Box>
-//       </Box>
-//     </Box>
-//   );
-// };
+        <Box flexBasis="26%" maxHeight="80vh" overflow="auto">
+          <EventVolunteer />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
-// export default VolunteerPage;
+export default VolunteerPage;
